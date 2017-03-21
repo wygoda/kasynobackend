95415624024c8f554974cf911c621be7b1238ebc
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-03-21 18:08:38 +0100

    added testApp

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^testapp/', include('testapp.urls'))
+    url(r'^testapp/', include('testApp.urls'))
     url(r'^admin/', include(admin.site.urls)),
 ]
