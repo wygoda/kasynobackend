701a2013d1b0986e98c50696f504068711de46eb
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-03-27 16:19:31 +0200

    testing of pushing to two remotes

--- wsgi/myproject/testApp/views.py
+++ wsgi/myproject/testApp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     if request.method == 'GET':
-        return HttpResponse("Hello, world. You're at the polls index and you used the GET method. hehe")
+        return HttpResponse("Hello, world. You're at the polls index and you used the GET method. hehe2")
     if request.method == 'POST':
         return HttpResponse("Hello, world. You're at the polls index and you used the POST method.")
 # Create your views here.
