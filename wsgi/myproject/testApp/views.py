a88d8e76f85ee358b6e62b16bd745cd24f150c5f
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-04 12:39:00 +0200

    i want to see what request looks like

--- wsgi/myproject/testApp/views.py
+++ wsgi/myproject/testApp/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 def index(request):
-    print('test2')
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    #output = ', '.join([p.question_text for p in latest_question_list])
+    template = loader.get_template('testapp/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(request)
+    #return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
