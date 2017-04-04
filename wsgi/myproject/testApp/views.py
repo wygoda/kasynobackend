61eec8f27f16c8c27fe56cc88ed92363a0dc22d0
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-04 12:16:36 +0200

    test2

--- wsgi/myproject/testApp/views.py
+++ wsgi/myproject/testApp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
+    print('test2')
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([p.question_text for p in latest_question_list])
     return HttpResponse(output)
