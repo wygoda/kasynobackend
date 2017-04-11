ba17bf71348b2a17d670c66e3d76edf1444e6703
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-11 18:57:16 +0200

    created backend, testing user api 2

--- wsgi/myproject/backend/models.py
+++ wsgi/myproject/backend/models.py
@@ -4,7 +4,7 @@ from decimal import *
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=50)
-    balance = models.Decimal(max_digits=13, decimal_places=2) #liczba wszystkich cyfr, cyfry po przecinku (dokladnosc)
+    balance = models.DecimalField(max_digits=13, decimal_places=2) #liczba wszystkich cyfr, cyfry po przecinku (dokladnosc)
 
     def modifyBalance(amount):
         try:
