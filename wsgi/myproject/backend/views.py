d7432f9f680ae4cdbce9837c5db48575a728abb4
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-11 19:32:55 +0200

    i just need to push to openshift repo to test

--- wsgi/myproject/backend/views.py
+++ wsgi/myproject/backend/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse, Http404
 
 def user(request):
     out = HttpResponse()
-    out.write('<b>ok</b>')
+    out.write('<b>ok user</b>')
+    return out
+
+def game(request):
+    out = HttpResponse()
+    out.write('<b>ok game</b>')
     return out
