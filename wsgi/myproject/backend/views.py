432144583b61e74d4bdcd954ba8886a697ae444b
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-11 20:11:18 +0200

    added backend app, created a user model, updated database, started working on user api

--- wsgi/myproject/backend/views.py
+++ wsgi/myproject/backend/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 
+@csrf_exempt #those views must accept post requests from external frontends done by the rest of the team, our own validation is required to prevent from csrf attacks
 def user(request):
     out = HttpResponse()
     out.write('<b>ok user</b>')
     return out
 
+@csrf_exempt
 def game(request):
     out = HttpResponse()
     out.write('<b>ok game</b>')
