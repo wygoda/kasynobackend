a4780d2da72356f2c37ab7e55cc1e4699f2717e7
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-12 14:13:37 +0200

    working on userapi

--- wsgi/myproject/backend/APIs/userapi.py
+++ wsgi/myproject/backend/APIs/userapi.py
@@ -2,13 +2,45 @@ from ..models import User
 from django.http import JsonResponse
 
 def register(request):
-    return JsonResponse ({'status':'register OK'})
+    username = request.POST.get('username')
+    notempty = User.objects.filter(username=username)
+    if notempty:
+        return JsonResponse({
+        'status':'FAIL',
+        'function':'register',
+        'error':'username already exists'})
+    password = request.POST.get('userPassword')
+
+    if username==None or password==None:
+        return JsonResponse({
+        'status':'FAIL',
+        'function':'register',
+        'error':'username or password empty'})
+    balance = request.POST.get('balance') #will return None if it doesn't find the key
+
+    if balance==None:
+        balance = 0
+
+    u = User(username=username, password=password, balance=balance)
+
+    try:
+        u.save()
+    except:
+        return JsonResponse({
+        'status':'FAIL',
+        'function':'register',
+        'error':'save() error'})
+
+    return JsonResponse({
+    'status':'OK',
+    'function':'register',
+    'id':u.id})
 
 def modifyBalance(request):
-    return JsonResponse ({'status':'modifyBalance OK'})
+    return JsonResponse ({'status':'OK'})
 
 def authenticate(request):
-    return JsonResponse ({'status':'authenticate OK'})
+    return JsonResponse ({'status':'OK'})
 
 def info(request):
-    return JsonResponse ({'status':'info OK'})
+    return JsonResponse ({'status':'OK'})
