a0cc4a9ceee672b6d21677e3ba75c4f82f650949
Maciej Wygoda <maciej.wygoda@gmail.com>
2017-04-13 13:22:25 +0200

    those -> these

--- wsgi/myproject/backend/views.py
+++ wsgi/myproject/backend/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .APIs import userapi
 import os
 
-@csrf_exempt #those views must accept post requests from external frontends done by the rest of the team, our own validation is required to prevent from csrf attacks
+@csrf_exempt #these views must accept post requests from external frontends done by the rest of the team, our own validation is required to prevent from csrf attacks
 def user(request):
     # out = HttpResponse()
     # out.write('<b>ok user</b>')
