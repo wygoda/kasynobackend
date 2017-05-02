0acd631b3e18157d3e9ccaaebfa47f2869756509
mSzadko <marcin_szadkowski@outlook.com>
2017-05-02 16:59:08 +0200

    hope it works now

--- wsgi/myproject/backend/APIs/checkapi.py
+++ wsgi/myproject/backend/APIs/checkapi.py
@@ -6,23 +6,23 @@ def slot(request):
 	col1 = request.POST.get('col1')
 	col2 = request.POST.get('col2')
 	col3 = request.POST.get('col3')
-	betamount = request.POST.get('betamount')
+	betamount = int(request.POST.get('betamount'))
 	if col1[1]==col2[1]==col3[1]:#jesli srodkowy wiersz ma 3 takie same znaczki
-		if col1[1]==1:#winogorona x2
+		if col1[1]=='1':#winogorona x2
 			betamount=betamount*2
-		if col1[1]==2:#wisnie x3
+		if col1[1]=='2':#wisnie x3
 			betamount=betamount*3
-		if col1[1]==3:#banany x4
+		if col1[1]=='3':#banany x4
 			betamount=betamount*4
-		if col1[1]==4:#gruszki x6
+		if col1[1]=='4':#gruszki x6
 			betamount=betamount*6
-		if col1[1]==5:#pomarancze x10
+		if col1[1]=='5':#pomarancze x10
 			betamount=betamount*10
-		if col1[1]==6:#serduszka x15
+		if col1[1]=='6':#serduszka x15
 			betamount=betamount*15
-		if col1[1]==7:#monety x20
+		if col1[1]=='7':#monety x20
 			betamount=betamount*20
-		if col1[1]==8:#jackpot x30
+		if col1[1]=='8':#jackpot x30
 			betamount=betamount*30
 	else: #przegrana
 		betamount=-betamount
