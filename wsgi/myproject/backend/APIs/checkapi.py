204daf3f63b68f9f24aceb503a4477b1ca249aac
mSzadko <marcin_szadkowski@outlook.com>
2017-05-02 15:41:15 +0200

    slot check check

--- wsgi/myproject/backend/APIs/checkapi.py
+++ wsgi/myproject/backend/APIs/checkapi.py
@@ -1,7 +1,34 @@
 from django.http import JsonResponse
+import userapi
 
 
 def slot(request):
+	col1 = request.POST.get('col1')
+	col2 = request.POST.get('col2')
+	col3 = request.POST.get('col3')
+	betamount = request.POST.get('betamount')
+	if col1[1]==col2[1]==col3[1]:#jesli srodkowy wiersz ma 3 takie same znaczki
+		if col1[1]==1:#winogorona x2
+			betamount=betamount*2
+		if col1[1]==2:#wisnie x3
+			betamount=betamount*3
+		if col1[1]==3:#banany x4
+			betamount=betamount*4
+		if col1[1]==4:#gruszki x6
+			betamount=betamount*6
+		if col1[1]==5:#pomarancze x10
+			betamount=betamount*10
+		if col1[1]==6:#serduszka x15
+			betamount=betamount*15
+		if col1[1]==7:#monety x20
+			betamount=betamount*20
+		if col1[1]==8:#jackpot x30
+			betamount=betamount*30
+	else: #przegrana
+		betamount=-betamount
+	callModifyBalance(request,betamount)
+	return JsonResponse({'amount':betamount})
+	
 	return JsonResponse({"status":"Slots not implemented"})
 def roulette(request):
 	return JsonResponse({"status":"Roulette not implemented"})
@@ -15,3 +42,13 @@ def baccarat(request):
 	return JsonResponse({"status":"Baccarat not implemented"})
 def cointoss(request):
 	return JsonResponse({"status":"Coin toss not implemented"})
+
+	
+	
+	
+def callModifyBalance(request,betamount):
+	mutable = request.POST._mutable
+	request.POST._mutable = True
+	request.POST['amount'] = betamount
+	requeest.POST._mutable = mutable
+	response=userapi.modifyBalance(request)
