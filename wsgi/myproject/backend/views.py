a002bbd3cd7fc0c70d62df605cf1126d4eac4e07
mSzadko <marcin_szadkowski@outlook.com>
2017-06-07 19:07:19 +0200

    faaffsjkdfhafa;

--- wsgi/myproject/backend/views.py
+++ wsgi/myproject/backend/views.py
@@ -12,38 +12,36 @@ def user(request):
 	# out = HttpResponse()
 	# out.write('<b>ok user</b>')
 	# return out
-	jsonObj = userapi.authenticate(request)
-	parsedJson = json.loads(jsonObj.content)
-	return JsonResponse({'status':parsedJson['status']})
-	#if request.method=='POST':
-	#	try:
-	#		function = request.POST.get('function')
-	#		if function==None:
-	#			return JsonResponse ({'status':'FAIL','error':'specify a function'})
-    #
-	#		if function=='register':
-	#			return userapi.register(request)
-    #
-	#		if function=='modifyBalance':
-	#			return userapi.modifyBalance(request)
-    #
-	#		if function=='authenticate':
-	#			return userapi.authenticate(request)
-    #
-	#		if function=='getAll':
-	#			return userapi.getAll(request)
-    #
-	#		if function=='getUser':
-	#			return userapi.getUser(request)
-    #
-	#		if function=='delete':
-	#			return userapi.delete(request)
-    #
-	#		return JsonResponse ({'status':'FAIL','error':'there\'s no such function'})
-	#	except:
-	#		return JsonResponse ({'status':'FAIL', 'error':'function execution went wrong'})
-	#else:
-	#	return JsonResponse({'status':'FAIL', 'error':'you didn\'t use POST or didn\'t pass validation'})
+
+	if request.method=='POST':
+		try:
+			function = request.POST.get('function')
+			if function==None:
+				return JsonResponse ({'status':'FAIL','error':'specify a function'})
+    
+			if function=='register':
+				return userapi.register(request)
+    
+			if function=='modifyBalance':
+				return userapi.modifyBalance(request)
+    
+			if function=='authenticate':
+				return userapi.authenticate(request)
+    
+			if function=='getAll':
+				return userapi.getAll(request)
+    
+			if function=='getUser':
+				return userapi.getUser(request)
+    
+			if function=='delete':
+				return userapi.delete(request)
+    
+			return JsonResponse ({'status':'FAIL','error':'there\'s no such function'})
+		except:
+			return JsonResponse ({'status':'FAIL', 'error':'function execution went wrong'})
+	else:
+		return JsonResponse({'status':'FAIL', 'error':'you didn\'t use POST or didn\'t pass validation'})
 
 @csrf_exempt
 def game(request):
