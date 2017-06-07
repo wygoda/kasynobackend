a82b60f0003dc3eaea557f60bddc79dda7182fd5
mSzadko <marcin_szadkowski@outlook.com>
2017-06-07 18:16:16 +0200

    .

--- wsgi/myproject/backend/views.py
+++ wsgi/myproject/backend/views.py
@@ -8,41 +8,41 @@ import os
 
 @csrf_exempt #these views must accept post requests from external frontends done by the rest of the team, our own validation is required to prevent from csrf attacks
 def user(request):
-    # out = HttpResponse()
-    # out.write('<b>ok user</b>')
-    # return out
-	#jsonObj = userapi.authenticate(request)
-	#jsonObj = json.load(jsonObj)
+	# out = HttpResponse()
+	# out.write('<b>ok user</b>')
+	# return out
+	jsonObj = userapi.authenticate(request)
+	jsonObj = json.load(jsonObj)
 	
-    if request.method=='POST':
-        try:
-            function = request.POST.get('function')
-            if function==None:
-                return JsonResponse ({'status':'FAIL','error':'specify a function'})
+	if request.method=='POST':
+		try:
+			function = request.POST.get('function')
+			if function==None:
+				return JsonResponse ({'status':'FAIL','error':'specify a function'})
 
-            if function=='register':
-                return userapi.register(request)
+			if function=='register':
+				return userapi.register(request)
 
-            if function=='modifyBalance':
-                return userapi.modifyBalance(request)
+			if function=='modifyBalance':
+				return userapi.modifyBalance(request)
 
-            if function=='authenticate':
-                return userapi.authenticate(request)
+			if function=='authenticate':
+				return userapi.authenticate(request)
 
-            if function=='getAll':
-                return userapi.getAll(request)
+			if function=='getAll':
+				return userapi.getAll(request)
 
-            if function=='getUser':
-                return userapi.getUser(request)
+			if function=='getUser':
+				return userapi.getUser(request)
 
-            if function=='delete':
-                return userapi.delete(request)
+			if function=='delete':
+				return userapi.delete(request)
 
-            return JsonResponse ({'status':'FAIL','error':'there\'s no such function'})
-        except:
-            return JsonResponse ({'status':'FAIL', 'error':'function execution went wrong'})
-    else:
-        return JsonResponse({'status':'FAIL', 'error':'you didn\'t use POST or didn\'t pass validation'})
+			return JsonResponse ({'status':'FAIL','error':'there\'s no such function'})
+		except:
+			return JsonResponse ({'status':'FAIL', 'error':'function execution went wrong'})
+	else:
+		return JsonResponse({'status':'FAIL', 'error':'you didn\'t use POST or didn\'t pass validation'})
 
 @csrf_exempt
 def game(request):
